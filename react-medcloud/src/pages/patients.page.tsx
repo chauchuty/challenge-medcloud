d3200284e0430ac7cfd007dbd650c33e8c88a1c4
chauchuty <chauchuty@alunos.utfpr.edu.br>
2022-04-15 17:15:53 -0300

    updated: update patient

--- react-medcloud/src/pages/patients.page.tsx
+++ react-medcloud/src/pages/patients.page.tsx
@@ -1,39 +1,14 @@
 import React, { useState, useEffect } from "react";
 import PatientService from "../services/patient.service";
-import Patient from "./../models/patient.model";
-
-function TableCustom(props: { patients: Patient[] }) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Nome</th>
-          <th>Data de Nascimento</th>
-          <th>Email</th>
-          <th>Endereço</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.patients.map((patient: Patient) => (
-          <tr key={patient.id}>
-            <td>{patient.id}</td>
-            <td>{patient.name}</td>
-            <td>{patient.birth_date.toLocaleString()}</td>
-            <td>{patient.email}</td>
-            <td>{patient.address}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+import TableCustom from "../components/medtable.custom";
+import { Button } from "@mui/material";
+import MedTable from "../components/medtable.custom";
 
 function PatientsPage() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [patients, setPatients] = useState([]);
   const service = new PatientService();
-
+ 
   useEffect(() => {
     service.getAll().then((data) => {
       setPatients(data);
@@ -41,11 +16,12 @@ function PatientsPage() {
     });
   }, []);
 
+  
   return !isLoaded ? (
     <div>Loading...</div>
   ) : (
     <div>
-      <TableCustom patients={patients} />
+      <MedTable patients={patients} setPatients={setPatients} service={service}/>
     </div>
   );
 }
